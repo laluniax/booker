06c407570ab86ed786d76a27fd4d264dc317b6ea
BYEONGTAEK <respect21047436@gmail.com>
2024-01-07 00:49:59 +0900

    로그인/회원가입 구현 완료/ 트리거사용

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from 'react';
+import { supabase } from '../../api/supabase';
 
 const Login = () => {
-  return <div>Login</div>;
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const [password, setPassword] = useState('');
+
+  //사용자 가입
+  const signInWithEmail = async () => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email:emailRef.current?.value || '',
+        password: password,
+      });
+
+      if (error) {
+        console.error('로그인중 오류 발생', error);
+      } else {
+        console.log('로그인 성공', data);
+
+        // 로그인 성공 후 추가 작업 수행
+        alert("로그인완료")
+      }
+    } catch (error) {
+      console.error('로그인중 오류 발생', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login </h2>
+      <label>Email:</label>
+      <input type="email" ref={emailRef} />
+      <label>Password:</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={signInWithEmail}>Sign Up</button>
+    </div>
+  );
 };
 
 export default Login;
