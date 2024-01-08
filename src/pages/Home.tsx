2f3848b7fcd5462ccb319519b56bf4bd4f5fa9f2
takoyakki0609 <kjy6478@naver.com>
2024-01-08 14:37:12 +0900

    서버

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+type Bestseller = {
+  bestRank: number;
+  title: string;
+  bestDuration: string;
+  author: string;
+  cover: string;
+  pubDate: string;
+  publisher: string;
+};
 
 const Home = () => {
-  return <div>Home</div>;
+  const [bestseller, setBestseller] = useState<Bestseller[]>([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const result = await axios.get('http://localhost:8080/bestseller');
+
+    setBestseller(result.data.item);
+  };
+
+  console.log(bestseller);
+
+  return (
+    <div>
+      {bestseller.map((item, idx) => {
+        return (
+          <div key={idx}>
+            <h2>{item.bestRank}</h2>
+            <h3>{item.title}</h3>
+            <img src={item.cover} alt="" />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
