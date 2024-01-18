825042abf37a5415f3c08d365e6e83d2d9df3e17
BYEONGTAEK <respect21047436@gmail.com>
2024-01-18 18:12:36 +0900

    feat: 중고거래 채팅 초기 기능 구현 및 수정 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 import { useEffect } from 'react';
-import { supabase } from './api/supabase.api';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { AuthContextProvider } from './contexts/auth.context';
 import Router from './shared/Router';
 import GlobalStyle from './styles/globalStyle';
 
-const App = () => {
-  useEffect(() => {
-    const changes = supabase
-      .channel('schema-db-changes')
-      .on(
-        'postgres_changes',
-        {
-          schema: 'public', // Subscribes to the "public" schema in Postgres
-          event: '*', // Listen to all changes
-        },
-        (payload) => console.log('app전역관리',payload),
-      )
-      .subscribe();
+const queryClient = new QueryClient();
 
-    return () => {
-      changes.unsubscribe();
-    };
-  }, []);
+const App = () => {
+  // useEffect(() => {
+  //   const changes = supabase
+  //     .channel('schema-db-changes')
+  //     .on(
+  //       'postgres_changes',
+  //       {
+  //         schema: 'public', // Subscribes to the "public" schema in Postgres
+  //         event: '*', // Listen to all changes
+  //       },
+  //       (payload) => console.log('app전역관리',payload),
+  //     )
+  //     .subscribe();
+
+  //   return () => {
+  //     changes.unsubscribe();
+  //   };
+  // }, []);
   return (
+    <QueryClientProvider client={queryClient}>
     <AuthContextProvider>
       <GlobalStyle />
       <Router />
     </AuthContextProvider>
+    </QueryClientProvider>
   );
 };
 
