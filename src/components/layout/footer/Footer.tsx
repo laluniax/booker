5a33ad537d6b4769a13eece0ae536a9a5a0c7d84
Parkjuwon <sogogizmt12@gmail.com>
2024-01-19 22:04:38 +0900

    design: MainPage CSS / Footer 작업중 / 민규님이 styles, src내부 assets 폴더 내 파일 -> public / images 폴더로 변경 작업 예정

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,8 +1,22 @@
-import React from "react";
-import * as St from "./FooterStyle";
+import * as St from './FooterStyle';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <St.FooterWrapper>
+      <St.FooterContentwrapper>
+        <St.LogoBox>
+          <St.Logo />
+        </St.LogoBox>
+
+        <St.FooterContentBox>
+          <p>Until 6</p>
+          <p>Leader : 천민규 </p>
+          <p></p>
+        </St.FooterContentBox>
+      </St.FooterContentwrapper>
+      <St.Copyrightcontent>ⓒ 2024 Until 6 final project All rights reserved</St.Copyrightcontent>
+    </St.FooterWrapper>
+  );
 };
 
 export default Footer;
