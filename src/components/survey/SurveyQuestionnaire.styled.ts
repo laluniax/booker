7758b01487884fb654b1b07e7aad4b38502b7e3d
yeonna18k <yeonna18k@gmail.com>
2024-01-30 01:32:20 +0900

    design: survey question mobile complete

--- src/components/survey/SurveyQuestionnaire.styled.ts
+++ src/components/survey/SurveyQuestionnaire.styled.ts
@@ -1,49 +1,41 @@
 import styled from 'styled-components';
 // 74c0fc d0ebff
-export const Container = styled.div`
-  min-height: 75rem;
-  width: 90rem;
+
+// 장르가 긴 설문(인기있는 책 장르별 추천받기 / 따끈따끈 신작 추천받기 / 텅장러 )의 컨테이너 입니다.
+export const LongContainer = styled.div`
+  min-height: 40rem;
+  max-width: 35rem;
   margin: 0 auto;
   text-align: center;
   display: flex;
   flex-direction: column;
   justify-content: center;
   font-family: 'GmarketSansMedium';
-  margin-bottom: 2rem;
-
-  &.genre {
-    height: 110vh;
-  }
-`;
-
-// 장르가 긴 설문(인기있는 책 장르별 추천받기 / 따끈따끈 신작 추천받기 / 텅장러 )의 컨테이너 입니다.
-export const LongContainer = styled.div`
-  min-height: 77rem;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  font-family: 'GmarketSansMedium';
+  padding-bottom: 5rem;
+  ${({ theme }) => theme.mediaQuery.sm`
+  min-height: 75rem;
+  max-width: 100rem;
   padding-bottom: 11rem;
+  `};
+  ${({ theme }) => theme.mediaQuery.lg`
+  max-width: 120rem;
+  `};
 `;
 
 // 장르가 긴 설문의 버튼 크기를 조절했습니다.
 // 1200px 에서 버튼 2개는 세로정렬 , 그 이상 넘어가면 가로로 정렬했습니다.
 export const LongAnswer = styled.div`
-  display: flex;
   margin: 2.3rem;
-  text-align: center;
-  justify-content: center;
   & button {
-    width: 20rem;
-    height: 7rem;
-    margin-left: 1rem;
+    width: 16rem;
+    height: 6rem;
+    margin-bottom: 1.5rem;
     font-family: 'GmarketSansMedium';
     color: #fff;
     border: 0.1rem solid #fca311;
     background-color: #fca311;
     border-radius: 2rem;
-    font-size: 2.5rem;
+    font-size: 2rem;
 
     &:hover {
       background-color: #14213d;
@@ -55,13 +47,61 @@ export const LongAnswer = styled.div`
       transition: 0.4s;
     }
   }
+  ${({ theme }) => theme.mediaQuery.sm`
+    display: flex;
+    text-align: center;
+    justify-content: center;
+
+  & button {
+    width: 15rem;
+    min-width: 8.5rem;
+    height: 7rem;
+    margin-left: 1rem;
+    font-size: 1.8rem;
+  }
+  `};
+  ${({ theme }) => theme.mediaQuery.lg`
+  & button {
+    width: 20rem;
+        font-size: 2.5rem;
+  }
+  `};
 `;
 
-export const Title = styled.div``;
-
 export const Question = styled.div`
-  font-size: 4rem;
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  ${({ theme }) => theme.mediaQuery.sm`
   margin-bottom: 5rem;
+  font-size: 3rem;
+  `};
+  ${({ theme }) => theme.mediaQuery.lg`
+  font-size: 4rem;
+  `};
+`;
+
+export const Container = styled.div`
+  min-height: 40rem;
+  max-width: 35rem;
+  margin: 0 auto;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-family: 'GmarketSansMedium';
+  margin-bottom: 2rem;
+
+  &.genre {
+    height: 110vh;
+  }
+
+  ${({ theme }) => theme.mediaQuery.sm`
+  min-height: 75rem;
+  max-width: 70rem;
+  `};
+  ${({ theme }) => theme.mediaQuery.lg`
+  max-width: 90rem;
+  `};
 `;
 
 export const Answer = styled.div`
@@ -69,12 +109,12 @@ export const Answer = styled.div`
 
   & button {
     font-family: 'GmarketSansMedium';
-    width: 30rem;
+    width: 20rem;
     color: #fff;
     border: 0.1rem solid #fca311;
     background-color: #fca311;
     border-radius: 2rem;
-    font-size: 3rem;
+    font-size: 2rem;
     padding: 3rem;
 
     &:hover {
@@ -87,4 +127,16 @@ export const Answer = styled.div`
       transition: 0.4s;
     }
   }
+  ${({ theme }) => theme.mediaQuery.sm`
+  & button {
+    width: 25rem;
+    font-size: 2.5rem;
+  }
+  `};
+  ${({ theme }) => theme.mediaQuery.lg`
+    & button {
+      width: 30rem;
+      font-size: 3rem;
+    }
+  `};
 `;
